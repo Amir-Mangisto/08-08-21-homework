bdd6f029bb0ec1101cf9422b8af914eef104032b
Amir-Mangisto <Mangisto.man>
2021-08-08 21:13:12 +0300

    homework

---
+++ main.js
@@ -0,0 +1,152 @@
+// function myName() {
+//     var myName = "amir";
+//     for (var i = 0; i < 5; i++) {
+//         console.log(myName);
+//     }
+// }
+// myName();
+
+// //question 2
+// function lastName(lastName) {
+
+//     for (var i = 0; i < 5; i++) {
+//         console.log(lastName)
+//     }
+// }
+// lastName("mangisto");
+// //question 3
+// function num(){
+//     var num1=55;
+//     var num2=10;
+//     console.log(num1 + num2);
+// }
+// num();
+// //question 4
+// function myNum(number1, number2){
+//      console.log(number1 + number2);
+// }
+// myNum(100 ,100);
+
+//question 5
+// function numbers(number1 , number2){
+//     if (number1 < number2)
+//     console.log(number1);
+//     else{
+//         console.log(number2)
+//     }
+// }
+// numbers(5 , 9);
+
+//question 6 ******undefind************
+// function myNumbers(num){
+// var num1 =num[0] 
+// var num2 =num [1]
+// var num3 =num[2]
+// return console.log(parseInt(num1), num2,num3);
+// }
+// myNumbers(123)
+
+
+//question 7
+// var sum = 0;
+// function numbers (myArray = [23,6,9,78,45]){
+//     for (var i=0; i<myArray.length ; i++){
+//       sum=myArray[i]+sum
+//     }
+// }
+// numbers()
+// console.log(sum)
+
+//question 7.2
+// var sum = 1;
+// function numbers (myArray = [23,6,9,78,45]){
+//     for (var i=0; i<myArray.length ; i++){
+//       sum=myArray[i]*sum
+//     }
+// }
+// numbers()
+// console.log(sum)
+
+//question 7.3
+// myArray = [23,6,9,78,45];
+// var min=myArray[0];
+// function numbers (num){
+//     for (var i=0; i<num.length;i++){
+//         if(min>num[i]){
+//             min=num[i];
+//         }
+//     }
+// console.log(min);
+// return min;
+// }
+//     numbers(myArray);
+    
+    
+// numbers();
+// console.log();
+
+//question 7.4
+
+// myArray = [23,6,9,78,45];
+// var max=myArray[0];
+// function numbers (num){
+//     for (var i=0; i<num.length;i++){
+//         if(max<num[i]){
+//             max=num[i];
+//         }
+//     }
+// console.log(max);
+// return max;
+// }
+//     numbers(myArray);
+    
+    
+// numbers();
+// console.log();
+
+//question 7.5
+// function numbers(){
+//     myArray = [23,6,9,78,45];
+//     for(var i=0; i<2; i++){
+//         myArray .pop();
+//     }
+//     console.log(myArray)
+
+// }
+// numbers();
+
+//question 8
+// var  nameArray="he said hello amir"; 
+// function myName(nameArray){
+//     console.log(nameArray.length);
+// }
+// myName(nameArray);
+
+
+//question 9
+
+// var myArray = [];
+// var userInput = Number (prompt ("insert a number"));
+
+// function userInput(){
+
+// }
+
+
+//question 10
+// var sum = 1;
+// numArray = [23,30,26,4,87];
+// function calculate(numArray=[23,30,26,4,87]){
+//     for(var i=0; i<numArray.length;i++){
+//         sum=numArray[i]*sum
+//     }
+// }
+// calculate();
+// console.log(sum);
+
+//question 11
+// var numbers=[1,20,3,1,47,8,3,55,20,6];
+function numbers(numbers){
+    if()
+}
+
